dc6491f57daf377fdfcaaa62df09d59e0f090df1
Barakat Egberinde <barakategberinde@gmail.com>
2025-11-09 06:40:51 +0100

    Converted workflows into functions

--- analyze.py
+++ analyze.py
@@ -1,13 +1,34 @@
-print("Welcome to the text analysis tool, I will mine and analyzes a body of text from a file you give me!")
+def welcomeuser():
+    print("Welcome to the text analysis tool, I will mine and analyzes a body of text from a file you give me!")
 
 #Get username
-
+def getusername():
+    # Get input user into the terminal
+    Greetings = input("\nTo begin, please enter your username:\n")
+    return Greetings
 # Print message prompting user to input their name
 #print("\nTo begin, please enter your username")
 
 
-# Get input user into the terminal
-Greetings = input("\nTo begin, please enter your username:\n")
+#Argument function
+def greetuser(Greetingsx, feelings): 
+    # Greet the user
+    print("Hello, " + Greetingsx + ", " + feelings)
+    #print("Hey, " + Greetingsx2)
 
-# Greet the user
-print("Hello, " + Greetings)
+#def textvariable():
+    #print("TESTING: " + Greetings)
+
+#def runprogram():
+
+welcomeuser()
+Greetings = getusername()
+#Greetings2 = getusername()
+greetuser(Greetings, "I miss you")
+#greetuser("How are you doing?")
+#textvariable()
+
+#runprogram()
+
+#Greetingsx is a local variable
+#Greetings is a global variable
