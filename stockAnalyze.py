1b944424c23dd10cda703d7ce77589021adaeb26
Barakat-20 <barakategberinde@gmail.com>
2026-01-07 14:25:21 +0100

    Got basic stock info

--- stockAnalyze.py
+++ stockAnalyze.py
@@ -1,18 +1,63 @@
 import yfinance as yf
+import time
+from datetime import datetime, date
+def extractBasicInfo(company):
+    data = company.fast_info
 
-def extractBasicInfo(data):
-    keysToExtract = [ 'longName', 'sector', 'fullTimeEmployees', 'marketCap', 'totalRevenue', 'trailingEps']
+    keysToExtract = [
+        'longName',
+        'sector',
+        'fullTimeEmployees',
+        'marketCap',
+        'totalRevenue',
+        'trailingEps'
+    ]
+
     basicInfo = {}
     for key in keysToExtract:
-        if key in data:
-            basicInfo[key] = data[key]
-        else:
-            basicInfo[key] = ""
+        basicInfo[key] = data.get(key, "")
+
     return basicInfo
 
-def getCompanyStockInfo(tickerSymbol):
-    company = yf.Ticker(tickerSymbol)
-    basicInfo = extractBasicInfo(company.info)
-    print(basicInfo)
+def getPriceHistory(company):
+    historyDf = company.history(period="12mo")
+    prices = historyDf['Open'].tolist()
+    dates = historyDf.index.strftime("%Y-%m-%d").tolist()
+    return {
+        'price': prices,
+        'date': dates
+    }
 
-getCompanyStockInfo("MSFT")
+def getEarningsDates(company):
+    earningDatesDf = company.earnings_dates
+    allDates = earningDatesDf.index.strftime("%Y-%m-%d").tolist()
+    date_objects = [datetime.strptime(d, "%Y-%m-%d") for d in allDates]
+    currentDate = datetime.now()
+    futureDates = [d for d in date_objects if d > currentDate]
+    futureDatesStr = [d.strftime("%Y-%m-%d") for d in futureDates]
+    return futureDatesStr
+
+def getCompanyNews(company):
+    newsList = company.news
+    allNewsArticles =[]
+    for newsDict in newsList:
+        content = newsDict.get('content', {})
+        newsDictToAdd = {
+            'title': content.get('title', ''),
+            'link': content.get('canonicalUrl', {}).get('url', '')
+        }
+        allNewsArticles.append(newsDictToAdd)
+    return allNewsArticles
+
+def getCompanyStockInfo(tickerSymbol):
+# Get data from Yahoo Finance API
+    company = yf.Ticker(tickerSymbol)
+
+# Get basic info on company  
+    basicInfo = extractBasicInfo(company)
+    time.sleep(2)
+    priceHistory = getPriceHistory(company)
+    futureEarningsDates = getEarningsDates(company)
+    newsArticles = getCompanyNews(company)
+
+getCompanyStockInfo('MSFT')
