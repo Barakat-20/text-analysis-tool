e596781a08c0bbc6f9bff81a9cc6ba9afb046aea
Barakat-20 <barakategberinde@gmail.com>
2026-01-08 08:49:50 +0100

    Extracted article text with bs4

--- stockAnalyze.py
+++ stockAnalyze.py
@@ -18,14 +18,14 @@ def extractBasicInfo(company):
 
     basicInfo = {}
     for key in keysToExtract:
-        basicInfo[key] = data.get(key, "")
+        basicInfo[key] = data.get(key, '')
 
     return basicInfo
 
 def getPriceHistory(company):
-    historyDf = company.history(period="12mo")
+    historyDf = company.history(period='12mo')
     prices = historyDf['Open'].tolist()
-    dates = historyDf.index.strftime("%Y-%m-%d").tolist()
+    dates = historyDf.index.strftime('%Y-%m-%d').tolist()
     return {
         'price': prices,
         'date': dates
@@ -33,11 +33,11 @@ def getPriceHistory(company):
 
 def getEarningsDates(company):
     earningDatesDf = company.earnings_dates
-    allDates = earningDatesDf.index.strftime("%Y-%m-%d").tolist()
-    date_objects = [datetime.strptime(d, "%Y-%m-%d") for d in allDates]
+    allDates = earningDatesDf.index.strftime('%Y-%m-%d').tolist()
+    date_objects = [datetime.strptime(d, '%Y-%m-%d') for d in allDates]
     currentDate = datetime.now()
     futureDates = [d for d in date_objects if d > currentDate]
-    futureDatesStr = [d.strftime("%Y-%m-%d") for d in futureDates]
+    futureDatesStr = [d.strftime('%Y-%m-%d') for d in futureDates]
     return futureDatesStr
 
 def getCompanyNews(company):
@@ -52,19 +52,31 @@ def getCompanyNews(company):
         allNewsArticles.append(newsDictToAdd)
     return allNewsArticles
 
+def extractNewsArticleTextFromHtml(soup):
+    allText = ''
+    result = soup.find_all('div',{'class':'article yf-1pvk4a5'})
+    for res in result:
+        allText += res.text
+    return allText
+
+
+
 headers = {
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36'
 }
 
 def extractCompanyNewsArticles(newsArticles):
+    allArticlesText = ''
     for newsArticle in newsArticles:
         url = newsArticle['link']
         page = requests.get(url, headers=headers)
         soup = BeautifulSoup(page.text, 'html.parser')
-        if soup.find_all(string="Continue reading"):
-            print("Tag found - should skip")
+        if not soup.find_all(string='Continue reading'):
+            print('Tag found - should skip')
         else:
-            print("Tag not found, don't skip")
+            print('Tag not found, don\'t skip')
+            allArticlesText += extractNewsArticleTextFromHtml(soup)
+    return allArticlesText
 
 def getCompanyStockInfo(tickerSymbol):
 # Get data from Yahoo Finance API
@@ -76,6 +88,7 @@ def getCompanyStockInfo(tickerSymbol):
     priceHistory = getPriceHistory(company)
     futureEarningsDates = getEarningsDates(company)
     newsArticles = getCompanyNews(company)
-    extractCompanyNewsArticles(newsArticles)
+    newsArticlesAllText = extractCompanyNewsArticles(newsArticles)
+    print(newsArticlesAllText)
 
 getCompanyStockInfo('MSFT')
