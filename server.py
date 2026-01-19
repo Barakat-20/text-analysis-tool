3ccb002b875da8a7cc0733ea9b0d016664e46aef
Barakat-20 <barakategberinde@gmail.com>
2026-01-19 13:26:12 +0100

    Prepared server for render deployment

--- server.py
+++ server.py
@@ -6,6 +6,9 @@ from flask_cors import CORS
 from stockAnalyze import getCompanyStockInfo
 from analyze import analyzeText
 
+f = open('test/result.json')
+stockDataTest = json.load(f)
+
 # Flask constructor takes the name of
 # current module (__name__) as argument.
 app = Flask(__name__)
@@ -20,6 +23,7 @@ def healthCheck():
 
 @app.route('/analyze-stock/<ticker>', methods=["GET"])
 def analyzeStock(ticker):
+    # return stockDataTest
     if len(ticker) > 5  or not ticker.isidentifier():
         abort(400, 'Invalid ticker symbol')
     try:
@@ -42,4 +46,4 @@ def analyzeTextHandler():
 if __name__ == '__main__':
     # run() method of Flask class runs the application
     # on the local development server.
-    app.run()
+    app.run(host="0.0.0.0")
